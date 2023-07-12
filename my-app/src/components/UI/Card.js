ab71602ddb9b22d8ccdc12cc581efe3610e47925
luckansone <lkravchuk2000@gmail.com>
2023-07-12 13:27:19 +0300

    added course 8 - sending http requests

--- my-app/src/components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Card.css'
-
-function Card(props) {
-    let classes = 'card ' + props.className;
-    return (<div className={classes}>{props.children}</div>)
-}
-
-export default Card;
